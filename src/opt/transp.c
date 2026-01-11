088f55e9dac84210e847f8fbef37cddf8236fda9
edwloef <edwin.frank.loeffler@gmail.com>
2026-01-11 20:19:22 +0100

    fix transp uid

--- src/opt/transp.c
+++ src/opt/transp.c
@@ -2,20 +2,20 @@
 
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
-void OCTCTinit(void) {}
+void OCTTinit(void) {}
 
-void OCTCTfini(void) {}
+void OCTTfini(void) {}
 
-node_st *OCTCTprogram(node_st *node) {
-    DATA_OCTCT_GET()->funtable = PROGRAM_FUNTABLE(node);
-    DATA_OCTCT_GET()->vartable = PROGRAM_VARTABLE(node);
+node_st *OCTTprogram(node_st *node) {
+    DATA_OCTT_GET()->funtable = PROGRAM_FUNTABLE(node);
+    DATA_OCTT_GET()->vartable = PROGRAM_VARTABLE(node);
 
     TRAVchildren(node);
 
     return node;
 }
 
-node_st *OCTCTexprs(node_st *node) {
+node_st *OCTTexprs(node_st *node) {
     TRAVchildren(node);
 
     EXPRS_TRANSP(node) = EXPR_TRANSP(EXPRS_EXPR(node)) &&
@@ -24,7 +24,7 @@ node_st *OCTCTexprs(node_st *node) {
     return node;
 }
 
-node_st *OCTCTarrexprs(node_st *node) {
+node_st *OCTTarrexprs(node_st *node) {
     TRAVchildren(node);
 
     ARREXPRS_TRANSP(node) =
@@ -34,22 +34,22 @@ node_st *OCTCTarrexprs(node_st *node) {
     return node;
 }
 
-node_st *OCTCTfundecl(node_st *node) {
-    int prev = DATA_OCTCT_GET()->min_nesting_level;
-    DATA_OCTCT_GET()->min_nesting_level = DATA_OCTCT_GET()->nesting_level;
-    DATA_OCTCT_GET()->vartable = FUNDECL_VARTABLE(node);
+node_st *OCTTfundecl(node_st *node) {
+    int prev = DATA_OCTT_GET()->min_nesting_level;
+    DATA_OCTT_GET()->min_nesting_level = DATA_OCTT_GET()->nesting_level;
+    DATA_OCTT_GET()->vartable = FUNDECL_VARTABLE(node);
 
     if (!FUNDECL_EXTERNAL(node)) {
-        DATA_OCTCT_GET()->min_nesting_level++;
+        DATA_OCTT_GET()->min_nesting_level++;
         TRAVchildren(node);
     }
 
-    int min_nesting_level = DATA_OCTCT_GET()->min_nesting_level;
-    DATA_OCTCT_GET()->min_nesting_level = MIN(prev, min_nesting_level);
-    DATA_OCTCT_GET()->vartable = DATA_OCTCT_GET()->vartable->parent;
+    int min_nesting_level = DATA_OCTT_GET()->min_nesting_level;
+    DATA_OCTT_GET()->min_nesting_level = MIN(prev, min_nesting_level);
+    DATA_OCTT_GET()->vartable = DATA_OCTT_GET()->vartable->parent;
 
     funtable_ref r = {0, FUNDECL_L(node)};
-    funtable_entry *e = funtable_get(DATA_OCTCT_GET()->funtable, r);
+    funtable_entry *e = funtable_get(DATA_OCTT_GET()->funtable, r);
     if (min_nesting_level != e->min_nesting_level) {
         e->min_nesting_level = min_nesting_level;
         CCNcycleNotify();
@@ -58,29 +58,29 @@ node_st *OCTCTfundecl(node_st *node) {
     return node;
 }
 
-node_st *OCTCTfunbody(node_st *node) {
-    DATA_OCTCT_GET()->nesting_level++;
-    DATA_OCTCT_GET()->funtable = FUNBODY_FUNTABLE(node);
+node_st *OCTTfunbody(node_st *node) {
+    DATA_OCTT_GET()->nesting_level++;
+    DATA_OCTT_GET()->funtable = FUNBODY_FUNTABLE(node);
 
     TRAVchildren(node);
 
-    DATA_OCTCT_GET()->nesting_level--;
-    DATA_OCTCT_GET()->funtable = DATA_OCTCT_GET()->funtable->parent;
+    DATA_OCTT_GET()->nesting_level--;
+    DATA_OCTT_GET()->funtable = DATA_OCTT_GET()->funtable->parent;
 
     return node;
 }
 
-node_st *OCTCTassign(node_st *node) {
+node_st *OCTTassign(node_st *node) {
     TRAVchildren(node);
 
-    DATA_OCTCT_GET()->min_nesting_level =
-        MIN(DATA_OCTCT_GET()->min_nesting_level,
-            DATA_OCTCT_GET()->nesting_level - VARREF_N(ASSIGN_REF(node)));
+    DATA_OCTT_GET()->min_nesting_level =
+        MIN(DATA_OCTT_GET()->min_nesting_level,
+            DATA_OCTT_GET()->nesting_level - VARREF_N(ASSIGN_REF(node)));
 
     return node;
 }
 
-node_st *OCTCTmonop(node_st *node) {
+node_st *OCTTmonop(node_st *node) {
     TRAVchildren(node);
 
     MONOP_TRANSP(node) = EXPR_TRANSP(MONOP_EXPR(node));
@@ -88,7 +88,7 @@ node_st *OCTCTmonop(node_st *node) {
     return node;
 }
 
-node_st *OCTCTbinop(node_st *node) {
+node_st *OCTTbinop(node_st *node) {
     TRAVchildren(node);
 
     BINOP_TRANSP(node) =
@@ -97,7 +97,7 @@ node_st *OCTCTbinop(node_st *node) {
     return node;
 }
 
-node_st *OCTCTcast(node_st *node) {
+node_st *OCTTcast(node_st *node) {
     TRAVchildren(node);
 
     CAST_TRANSP(node) = EXPR_TRANSP(CAST_EXPR(node));
@@ -105,25 +105,25 @@ node_st *OCTCTcast(node_st *node) {
     return node;
 }
 
-node_st *OCTCTcall(node_st *node) {
+node_st *OCTTcall(node_st *node) {
     TRAVchildren(node);
 
     funtable_ref r = {CALL_N(node), CALL_L(node)};
-    bool fun_transp = funtable_transp(DATA_OCTCT_GET()->funtable, r);
+    bool fun_transp = funtable_transp(DATA_OCTT_GET()->funtable, r);
 
     CALL_TRANSP(node) =
         fun_transp && (!CALL_EXPRS(node) || EXPRS_TRANSP(CALL_EXPRS(node)));
 
     if (!fun_transp) {
-        DATA_OCTCT_GET()->min_nesting_level =
-            MIN(DATA_OCTCT_GET()->min_nesting_level,
-                DATA_OCTCT_GET()->nesting_level - CALL_N(node));
+        DATA_OCTT_GET()->min_nesting_level =
+            MIN(DATA_OCTT_GET()->min_nesting_level,
+                DATA_OCTT_GET()->nesting_level - CALL_N(node));
     }
 
     return node;
 }
 
-node_st *OCTCTvarref(node_st *node) {
+node_st *OCTTvarref(node_st *node) {
     TRAVchildren(node);
 
     VARREF_TRANSP(node) =
@@ -132,19 +132,19 @@ node_st *OCTCTvarref(node_st *node) {
     return node;
 }
 
-node_st *OCTCTint(node_st *node) {
+node_st *OCTTint(node_st *node) {
     INT_TRANSP(node) = true;
 
     return node;
 }
 
-node_st *OCTCTfloat(node_st *node) {
+node_st *OCTTfloat(node_st *node) {
     FLOAT_TRANSP(node) = true;
 
     return node;
 }
 
-node_st *OCTCTbool(node_st *node) {
+node_st *OCTTbool(node_st *node) {
     BOOL_TRANSP(node) = true;
 
     return node;
