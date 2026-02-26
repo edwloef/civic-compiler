6e07da672e1a7627431c8d62ebda415a7be4b65c
edwloef <edwin.frank.loeffler@gmail.com>
2026-02-26 23:22:46 +0100

    clean up forloops

--- src/desugar/forloops.c
+++ src/desugar/forloops.c
@@ -77,111 +77,120 @@ node_st *DFLstmts(node_st *node) {
     TRAVchildren(node);
 
     node_st *stmt = STMTS_STMT(node);
-    if (NODE_TYPE(stmt) == NT_FOR) {
-        node_st *start_ref = FOR_REF(stmt);
-        node_st *end_ref;
-        node_st *step_ref;
-        node_st *clamped_end_ref = NULL;
+    if (!stmt || NODE_TYPE(stmt) == NT_STMTS) {
+        STMTS_STMT(node) = NULL;
+        TAKE(STMTS_NEXT(node));
+        node = inline_stmts(node, stmt);
+    }
 
-        node_st *start_assign = ASTassign(start_ref, FOR_LOOP_START(stmt));
-        node_st *end_assign = NULL;
-        node_st *step_assign = NULL;
+    return node;
+}
 
-        if (NODE_TYPE(FOR_LOOP_END(stmt)) == NT_INT) {
-            end_ref = FOR_LOOP_END(stmt);
-        } else {
-            end_ref = vartable_temp_var(DATA_DFL_GET()->vartable, TY_int);
-            end_assign = ASTassign(end_ref, FOR_LOOP_END(stmt));
-        }
+node_st *DFLfor(node_st *node) {
+    TRAVchildren(node);
 
-        if (NODE_TYPE(FOR_LOOP_STEP(stmt)) == NT_INT) {
-            step_ref = FOR_LOOP_STEP(stmt);
-        } else {
-            step_ref = vartable_temp_var(DATA_DFL_GET()->vartable, TY_int);
-            step_assign = ASTassign(step_ref, FOR_LOOP_STEP(stmt));
-        }
+    node_st *start_ref = FOR_REF(node);
+    node_st *end_ref;
+    node_st *step_ref;
+    node_st *clamped_end_ref = NULL;
 
-        if (NODE_TYPE(step_ref) == NT_INT) {
-            if (INT_VAL(step_ref) >= -1 && INT_VAL(step_ref) <= 1) {
-            } else if (NODE_TYPE(end_ref) == NT_INT) {
-                if (INT_VAL(step_ref) > 0 &&
-                    INT_VAL(end_ref) + INT_VAL(step_ref) > INT_VAL(end_ref)) {
-                } else if (INT_VAL(step_ref) < 0 &&
-                           INT_VAL(end_ref) + INT_VAL(step_ref) <
-                               INT_VAL(end_ref)) {
-                } else {
-                    clamped_end_ref =
-                        ASTint(INT_VAL(end_ref) - INT_VAL(step_ref), TY_int);
-                }
+    node_st *start_assign = ASTassign(start_ref, FOR_LOOP_START(node));
+    node_st *end_assign = NULL;
+    node_st *step_assign = NULL;
+
+    if (NODE_TYPE(FOR_LOOP_END(node)) == NT_INT) {
+        end_ref = FOR_LOOP_END(node);
+    } else {
+        end_ref = vartable_temp_var(DATA_DFL_GET()->vartable, TY_int);
+        end_assign = ASTassign(end_ref, FOR_LOOP_END(node));
+    }
+
+    if (NODE_TYPE(FOR_LOOP_STEP(node)) == NT_INT) {
+        step_ref = FOR_LOOP_STEP(node);
+    } else {
+        step_ref = vartable_temp_var(DATA_DFL_GET()->vartable, TY_int);
+        step_assign = ASTassign(step_ref, FOR_LOOP_STEP(node));
+    }
+
+    if (NODE_TYPE(step_ref) == NT_INT) {
+        if (INT_VAL(step_ref) >= -1 && INT_VAL(step_ref) <= 1) {
+        } else if (NODE_TYPE(end_ref) == NT_INT) {
+            if (INT_VAL(step_ref) > 0 &&
+                INT_VAL(end_ref) + INT_VAL(step_ref) > INT_VAL(end_ref)) {
+            } else if (INT_VAL(step_ref) < 0 &&
+                       INT_VAL(end_ref) + INT_VAL(step_ref) <
+                           INT_VAL(end_ref)) {
             } else {
                 clamped_end_ref =
-                    vartable_temp_var(DATA_DFL_GET()->vartable, TY_int);
+                    ASTint(INT_VAL(end_ref) - INT_VAL(step_ref), TY_int);
             }
         } else {
             clamped_end_ref =
                 vartable_temp_var(DATA_DFL_GET()->vartable, TY_int);
         }
-
-        node_st *inc = ASTassign(
-            CCNcopy(start_ref),
-            ASTbinop(CCNcopy(start_ref), CCNcopy(step_ref), BO_add, TY_int));
-        VARREF_WRITE(ASSIGN_REF(inc)) = true;
-
-        FOR_STMTS(stmt) = inline_stmts(ASTstmts(inc, NULL), FOR_STMTS(stmt));
-
-        FOR_REF(stmt) = NULL;
-        FOR_LOOP_START(stmt) = NULL;
-        FOR_LOOP_END(stmt) = NULL;
-        FOR_LOOP_STEP(stmt) = NULL;
-
-        node_st *positive_step = NULL;
-        node_st *negative_step = NULL;
-
-        if (!(NODE_TYPE(step_ref) == NT_INT && INT_VAL(step_ref) <= 0)) {
-            positive_step = DFLfor_branch(stmt, start_ref, end_ref, step_ref,
-                                          clamped_end_ref, BO_lt);
-        }
-
-        if (!(NODE_TYPE(step_ref) == NT_INT && INT_VAL(step_ref) >= 0)) {
-            negative_step = DFLfor_branch(stmt, start_ref, end_ref, step_ref,
-                                          clamped_end_ref, BO_gt);
-        }
-
-        TAKE(STMTS_NEXT(node));
-
-        node_st *transformed;
-        if (!positive_step) {
-            transformed = negative_step;
-        } else if (!negative_step) {
-            transformed = positive_step;
-        } else {
-            transformed =
-                ASTstmts(ASTifelse(ASTbinop(CCNcopy(step_ref),
-                                            ASTint(0, TY_int), BO_gt, TY_bool),
-                                   positive_step, negative_step),
-                         NULL);
-        }
-        node = inline_stmts(node, transformed);
-
-        CCNfree(clamped_end_ref);
-
-        if (step_assign) {
-            VARREF_WRITE(step_ref) = true;
-            node = ASTstmts(step_assign, node);
-        } else {
-            CCNfree(step_ref);
-        }
-
-        if (end_assign) {
-            VARREF_WRITE(end_ref) = true;
-            node = ASTstmts(end_assign, node);
-        } else {
-            CCNfree(end_ref);
-        }
-
-        VARREF_WRITE(start_ref) = true;
-        node = ASTstmts(start_assign, node);
+    } else {
+        clamped_end_ref = vartable_temp_var(DATA_DFL_GET()->vartable, TY_int);
     }
+
+    node_st *inc = ASTassign(
+        CCNcopy(start_ref),
+        ASTbinop(CCNcopy(start_ref), CCNcopy(step_ref), BO_add, TY_int));
+    VARREF_WRITE(ASSIGN_REF(inc)) = true;
+
+    FOR_STMTS(node) = inline_stmts(ASTstmts(inc, NULL), FOR_STMTS(node));
+
+    FOR_REF(node) = NULL;
+    FOR_LOOP_START(node) = NULL;
+    FOR_LOOP_END(node) = NULL;
+    FOR_LOOP_STEP(node) = NULL;
+
+    node_st *positive_step = NULL;
+    node_st *negative_step = NULL;
+
+    if (!(NODE_TYPE(step_ref) == NT_INT && INT_VAL(step_ref) <= 0)) {
+        positive_step = DFLfor_branch(node, start_ref, end_ref, step_ref,
+                                      clamped_end_ref, BO_lt);
+    }
+
+    if (!(NODE_TYPE(step_ref) == NT_INT && INT_VAL(step_ref) >= 0)) {
+        negative_step = DFLfor_branch(node, start_ref, end_ref, step_ref,
+                                      clamped_end_ref, BO_gt);
+    }
+
+    node = CCNfree(node);
+
+    node_st *transformed;
+    if (!positive_step) {
+        transformed = negative_step;
+    } else if (!negative_step) {
+        transformed = positive_step;
+    } else {
+        transformed =
+            ASTstmts(ASTifelse(ASTbinop(CCNcopy(step_ref), ASTint(0, TY_int),
+                                        BO_gt, TY_bool),
+                               positive_step, negative_step),
+                     NULL);
+    }
+    node = inline_stmts(node, transformed);
+
+    CCNfree(clamped_end_ref);
+
+    if (step_assign) {
+        VARREF_WRITE(step_ref) = true;
+        node = ASTstmts(step_assign, node);
+    } else {
+        CCNfree(step_ref);
+    }
+
+    if (end_assign) {
+        VARREF_WRITE(end_ref) = true;
+        node = ASTstmts(end_assign, node);
+    } else {
+        CCNfree(end_ref);
+    }
+
+    VARREF_WRITE(start_ref) = true;
+    node = ASTstmts(start_assign, node);
 
     return node;
 }
